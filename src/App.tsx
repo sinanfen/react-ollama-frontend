57990e2f0768d9eecf5a860ea7c66c9f8c70e1b6
Sinan Fen <sinanfen@hotmail.com>
2024-08-03 22:18:26 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import ChatUI from "./components/ChatUI.tsx";
+
+function App() {
+  return (
+    <>
+      <div className="app">
+        <ChatUI />
+      </div>
+    </>
+  );
+}
+
+export default App;
